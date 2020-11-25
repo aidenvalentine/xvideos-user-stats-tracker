27fa7a2bcaa91075b6b810cd0b4db17c3365ce8b
Aiden Valentine <aiden@xxxmultimedia.com>
2020-11-25 10:06:04 -0500

    Switch to Puppeteer instead of request/cheerio

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const request = require("request");
-const cheerio = require("cheerio");
+const puppeteer = require('puppeteer');
+const request = require('request-promise-native').defaults({
+  // proxy: 'http://username:password@host:port', // Fixes potential timeout issues.
+  strictSSL: false // Fixes potential timeout issues.
+});
 const Influx = require("influx");
 
-// Set your variables
-var influxDbName = "xvideos_aiden_valentine_stats_db";
-var xvideosProfileUrl = "https://www.xvideos.com/pornstar-channels/aidenvalentineofficial";
+// Get user config from .env file
+require('dotenv').config()
+
+// Grab your variables from the .env file. Make sure these values are set!
+const dbName = process.env.DB_NAME || "xvideos_aiden_valentine_stats_db";
+const dbHost = process.env.DB_HOST || "127.0.0.1";
+const dbUser = process.env.DB_USER || "";
+const dbPass = process.env.DB_PASS || "";
+const dbPort = process.env.DB_PORT || 8086;
 
 // Influx data model for Xvideos User Statistics
 var influx = new Influx.InfluxDB({
   host: '127.0.0.1',
-  database: influxDbName,
+  database: dbName,
   username: '',
   password: '',
   port: 8086,
@@ -43,128 +52,107 @@ var influx = new Influx.InfluxDB({
   }
 })
 
-var metadata = {}; // Create global variable.
 
-// Send web crawler to user's profile page
-request(xvideosProfileUrl, function(error, response, html) {
-  if (!error && response.statusCode == 200) {
-    var $ = cheerio.load(html);
+// Make our browser look less like a robot! An ounce of prevention...
+const args = [
+  '--no-sandbox',
+  '--disable-setuid-sandbox',
+  '--disable-infobars',
+  '--window-position=0,0',
+  '--ignore-certifcate-errors',
+  '--ignore-certifcate-errors-spki-list',
+  '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
+];
 
+const chromeOptions = {
+  args,
+  executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+  headless: false, // To debug, set to true and it'll show the browser window.
+  slowMo: 10,
+  defaultViewport: null
+};
+
+(async function main() {
+  const browser = await puppeteer.launch(chromeOptions);
+
+  const page = await browser.newPage();
+
+  await page.goto('https://www.xvideos.com/pornstar-channels/aidenvalentineofficial#_tabAboutMe', {waitUntil: 'networkidle2'});
+
+  // var metadata = {}; // Create global variable.
+
+  // Fetch data
+  var metadata = await page.evaluate(() => {
+    var metadata = {}; // Create global variable.
     // Grab variables from the document
     metadata.profileHits = parseInt($("#pinfo-profile-hits > span").text().replace(/,/g, ""));
     metadata.subscribers = parseInt($("#pinfo-subscribers > span").text().replace(/,/g, ""));
     metadata.videosViews = parseInt($("#pinfo-videos-views > span").text().replace(/,/g, ""));
     metadata.pornstarViews = parseInt($("#pinfo-pornstar-views > span").text().replace(/,/g, ""));
-    metadata.pornActorWorldRanking = parseInt($("#pinfo-pornstar-ranks > span:nth-child(3) > strong > a").text().replace(/,/g, ""));
-    metadata.pornActorContinentRanking = parseInt($("#pinfo-pornstar-ranks > span:nth-child(5) > strong > a").text().replace(/,/g, ""));
-    metadata.pornActorCountryRanking = parseInt($("#pinfo-pornstar-ranks > span:nth-child(7) > strong > a").text().replace(/,/g, ""));
-    metadata.channelWorldRanking = parseInt($("#pinfo-channel-ranks > span:nth-child(3) > strong > a").text().replace(/,/g, ""));
-    metadata.channelContinentRanking = parseInt($("#pinfo-channel-ranks > span:nth-child(5) > strong > a").text().replace(/,/g, ""));
-    metadata.channelCountryRanking = parseInt($("#pinfo-channel-ranks > span:nth-child(7) > strong > a").text().replace(/,/g, ""));
-    metadata.maleGlobalWorldRanking = parseInt($("#pinfo-pornstar-global-ranks > span:nth-child(3) > strong > a").text().replace(/,/g, ""));
-    metadata.maleGlobalContinentRanking = parseInt($("#pinfo-pornstar-global-ranks > span:nth-child(5) > strong > a").text().replace(/,/g, ""));
-    metadata.maleGlobalCountryRanking = parseInt($("#pinfo-pornstar-global-ranks > span:nth-child(7) > strong > a").text().replace(/,/g, ""));
+    metadata.pornActorWorldRanking = parseInt($("#pfinfo-col-col1 > p:nth-child(10) > span:nth-child(3) > strong > a").text().replace(/,/g, ""));
+    metadata.pornActorContinentRanking = parseInt($("#pfinfo-col-col1 > p:nth-child(10) > span:nth-child(5) > strong > a").text().replace(/,/g, ""));
+    metadata.pornActorCountryRanking = parseInt($("#pfinfo-col-col1 > p:nth-child(10) > span:nth-child(7) > strong > a").text().replace(/,/g, ""));
+    metadata.channelWorldRanking = parseInt($("#pfinfo-col-col1 > p:nth-child(13) > span:nth-child(3) > strong > a").text().replace(/,/g, ""));
+    metadata.channelContinentRanking = parseInt($("#pfinfo-col-col1 > p:nth-child(13) > span:nth-child(5) > strong > a").text().replace(/,/g, ""));
+    metadata.channelCountryRanking = parseInt($("#pfinfo-col-col1 > p:nth-child(13) > span:nth-child(7) > strong > a").text().replace(/,/g, ""));
+    metadata.maleGlobalWorldRanking = parseInt($("#pfinfo-col-col1 > p:nth-child(16) > span:nth-child(3) > strong > a").text().replace(/,/g, ""));
+    metadata.maleGlobalContinentRanking = parseInt($("#pfinfo-col-col1 > p:nth-child(16) > span:nth-child(5) > strong > a").text().replace(/,/g, ""));
+    metadata.maleGlobalCountryRanking = parseInt($("#pfinfo-col-col1 > p:nth-child(16) > span:nth-child(7) > strong > a").text().replace(/,/g, ""));
     // var numberOfVideos = $("#tabAboutMe > h4 > a > strong").text().replace(/,/g, "");
     // var votes = $("#pfinfo-col-pict > div.rating-box > div > span").text().replace(/,/g, "");
     // console.log(votes);
     metadata.friends = parseInt($("#tab-friends > span.navbadge").text().split("/")[0].replace(/,/g, "").replace(/\s/g, ""));
     metadata.fans = parseInt($("#tab-friends > span.navbadge").text().split("/")[1].replace(/,/g, "").replace(/\s/g, ""));
     metadata.username = $("#profile-title > h2 > strong").text();
+    metadata.numberOfVideos = parseInt($("#tabAboutMe > h4 > a > strong").text()); // TEMP - This is not the most accurate count.
+    console.log(metadata);
+    return metadata;
+  });
 
-    // console.log(metadata); // debug
+  // await page.goto('https://www.xvideos.com/pornstar-channels/aidenvalentineofficial#_tabVideos,videos-new');
 
-    // Send web crawler to videos page to get the updated # of videos. (More accurate than profile page)
-    request(xvideosProfileUrl + "#_tabVideos,videos-new", function(error, response, html) {
-      if (!error && response.statusCode == 200) {
-        var $ = cheerio.load(html);
-        metadata.numberOfVideos = parseInt($("#tabAboutMe > h4 > a > strong").text());
-        console.log(metadata); // debug
-        // metadata.numberOfVideos = metadata.numberOfVideos.match(/\d+/g)[0]; // Search resulting text for digits
+  console.log('Data received');
+  // var data = await response.detail.json();
+  console.log("Adding/updating row in database\n", JSON.stringify(metadata, 2, 2));
+  // Write data to database
+  influx.writePoints([{
+      measurement: 'user_stats',
+      tags: {
+        username: metadata.username,
+      },
+      fields: {
+        profileHits: metadata.profileHits,
+        subscribers: metadata.subscribers,
+        videosViews: metadata.videosViews,
+        pornstarViews: metadata.pornstarViews,
+        pornActorWorldRanking: metadata.pornActorWorldRanking,
+        pornActorContinentRanking: metadata.pornActorContinentRanking,
+        pornActorCountryRanking: metadata.pornActorCountryRanking,
+        channelWorldRanking: metadata.channelWorldRanking,
+        channelContinentRanking: metadata.channelContinentRanking,
+        channelCountryRanking: metadata.channelCountryRanking,
+        maleGlobalWorldRanking: metadata.maleGlobalWorldRanking,
+        maleGlobalContinentRanking: metadata.maleGlobalContinentRanking,
+        maleGlobalCountryRanking: metadata.maleGlobalCountryRanking,
+        numberOfVideos: metadata.numberOfVideos,
+        // votes: metadata.votes,
+        friends: metadata.friends,
+        fans: metadata.fans,
+      },
+      // timestamp: new Date().getTime(),
+    }], {
+      database: dbName,
+      precision: 's',
+    })
+    .then(function() {
+      console.log("Success!");
+      page.close();
+      process.exit(0); // Success
+    })
+    .catch(error => {
+      console.error(`Error saving data to InfluxDB! ${error}`)
+      page.close();
+      process.exit(1); // Failure
+    })
 
-        // Write data to database
-        influx.writePoints([{
-            measurement: 'user_stats',
-            tags: {
-              username: metadata.username,
-            },
-            fields: {
-              profileHits: metadata.profileHits,
-              subscribers: metadata.subscribers,
-              videosViews: metadata.videosViews,
-              pornstarViews: metadata.pornstarViews,
-              pornActorWorldRanking: metadata.pornActorWorldRanking,
-              pornActorContinentRanking: metadata.pornActorContinentRanking,
-              pornActorCountryRanking: metadata.pornActorCountryRanking,
-              channelWorldRanking: metadata.channelWorldRanking,
-              channelContinentRanking: metadata.channelContinentRanking,
-              channelCountryRanking: metadata.channelCountryRanking,
-              maleGlobalWorldRanking: metadata.maleGlobalWorldRanking,
-              maleGlobalContinentRanking: metadata.maleGlobalContinentRanking,
-              maleGlobalCountryRanking: metadata.maleGlobalCountryRanking,
-              numberOfVideos: metadata.numberOfVideos,
-              // votes: metadata.votes,
-              friends: metadata.friends,
-              fans: metadata.fans,
-            },
-            // timestamp: tidePoint.epoch,
-          }], {
-            database: influxDbName,
-            precision: 's',
-          })
-          .catch(error => {
-            console.error(`Error saving data to InfluxDB! ${err.stack}`)
-          }).then(() => {
-            // Get last 2 entries from database to compare the video count.
-            return influx.query(`
-                select numberOfVideos from user_stats
-                order by time desc
-                limit 2
-              `)
-          }).then(rows => {
-            rows.forEach(row => console.log(`${row.numberOfVideos} videos at ${row.time}`)); // debug
-            // See if the video count has increase. If so, add annotations to Grafana dashboard.
-            if (rows[0].numberOfVideos > rows[1].numberOfVideos) {
-              var newVideosCount = rows[0].numberOfVideos - rows[1].numberOfVideos;
-              console.log(newVideosCount + " new videos.");
-
-              // Grafana create annotation api request body
-              var data = {
-                "time": rows[0].time.getNanoTime()/1000000, // Convert to Unix epoch seconds
-                "timeEnd": rows[0].time.getNanoTime()/1000000, // Convert to Unix epoch seconds
-                "tags": ["release"],
-                "text": `${newVideosCount} new releases`
-              }
-
-              // Request settings
-              const options = {
-                url: 'http://admin:admin@localhost:3001/api/annotations',
-                headers: {
-                  "User-Agent": 'request',
-                  "content-type": 'application/json'
-                },
-                body: data,
-                json: true
-              };
-
-              function addAnnotation(dashboardId, panelId) {
-                options.body.dashboardId = dashboardId;
-                options.body.panelId = panelId;
-                console.log(options);
-                request.post(options, function(error, response, html) {
-                  if (!error && response.statusCode == 200) {
-                    console.log(response.body);
-                  } else {
-                    console.error("Error adding annotation.", error, response.body);
-                  }
-                });
-              };
-              addAnnotation(1, 2); // Subscribers
-              addAnnotation(1, 27); // Profile Hits
-              addAnnotation(1, 28); // Total Views
-              addAnnotation(1, 29); // Own Uploads Views
-            }
-          })
-        }
-      })
-  }
-});
+})()
